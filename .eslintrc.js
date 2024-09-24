0fbc7e54d49831fbd0b163676c43541dc4d7e9ed
kuku20 <lluu3790@gmail.com>
2024-09-23 23:56:11 -0500

    es

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,11 @@ module.exports = {
     ecmaVersion: 'latest'
   },
   rules: {
+    'semi': ['error', 'never'], // No semicolon required at the end of statements
+    'comma-spacing': ['error', { 'before': false, 'after': true }], // A space after a comma
+    'no-multiple-empty-lines': ['error', { 'max': 1 }], // No two blank lines
+    'indent': ['error', 2], // Only two spaces for indentation
+    'quotes': ['error', 'single'], // Strings should use single quotes
+    'eqeqeq': ['error', 'always'] // Enforce the use of === and !== instead of == and !=
   }
 }
